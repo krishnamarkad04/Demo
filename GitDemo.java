01fa67aa9fd7757626455ee8cdf0edc71ac6c9d5
krishnamrkad04 <krishnamrkad04@gmail.com>
2023-01-28 10:54:51 +0530

    firstcommit

---
+++ GitDemo.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+package 1;
+class Demo1
+{
+public void m1()
+{
+System.out.print("Today 27.01.2023 I opened Git account");
+}
+}
+class GitDemo
+{
+public static void main(String[] args)
+{
+Demo1 n=new Demo1();
+n.m1();
+}
+}
